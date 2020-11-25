ab0877ce4cd2212f6e4fdfc72050b3aa502b19f6
haru1801 <haru1801@student.miun.se>
2020-11-25 15:30:46 +0000

    Lagt meira afturat í roknskapinum.

---
+++ XBRLviewerMVC/ViewComponents/tdFiller.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Html;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewComponents;
+
+namespace XBRLviewerMVC.ViewComponents
+{
+    public class tdFiller
+    {
+        public tdFiller()
+        {
+
+        }
+
+        public IViewComponentResult Invoke()
+        {
+            return new HtmlContentViewComponentResult(new HtmlString("hello - <b>Yes, Hello</b>"));
+        }
+    }
+}
